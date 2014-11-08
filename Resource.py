7837fb680d7ff5f2e4c016aaaad7e625251a92c6
miguel5 <miguel.amigot@gmail.com>
2014-11-08 12:29:40 -0500

    Methods to manage resources' units

--- Resource.py
+++ Resource.py
@@ -1,7 +1,33 @@
 class Resource:
 
-    def __init__(self, id, totUnits):
+    def __init__(self, id, numUnits):
         self.id = id
+        self.numUnits = numUnits
+
+        self.availableUnits = numUnits
+        self.busyUnits = 0
+
+    def takeUnit(self):
+        if( self.availableUnits > 0 )
+            self.availableUnits -= 1
+            self.busyUnits += 1
+
+    def freeUnit(self):
+        if( self.availableUnits + 1 <= self.numUnits )
+            self.availableUnits += 1
+            self.busyUnits -= 1
+
+    def getID(self):
+        return self.id
+
+    def getNumUnits(self):
+        return self.numUnits
+
+    def getNumAvailable(self):
+        return self.availableUnits
+
+    def getNumBusy(self):
+        return self.busyUnits
 
     def __repr__(self):
         return str(self.__dict__)
