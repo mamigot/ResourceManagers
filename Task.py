532ed20f146556472248d775eadb20790461a292
miguel5 <miguel.amigot@gmail.com>
2014-11-08 14:00:06 -0500

    Book-keeping methods to keep track of resource unit counts

--- Task.py
+++ Task.py
@@ -7,11 +7,35 @@ class Task:
         self.finished = False
         self.currInstruction = 0 # Used to iterate through instructions
 
+        self.heldResources = {} # Maps resource ID to count of units
+
+
     def isFinished(self):
         return self.finished
 
+    def grantResource(self, resourceID, numUnits=1):
+        '''
+        Does not check if the resource actually has the units
+        (that's the manager's job --this is just book-keeping)
+        '''
+        if( resourceID in self.heldResources.keys() )
+            # Already have at least one unit of this resource
+            self.heldResources[resourceID] += numUnits
+        else
+            self.heldResources[resourceID] = numUnits
+
+    def releaseResource(self, resourceID, numUnits=1):
+        if( resourceID in self.heldResources.keys() )
+            self.heldResources[resourceID] -= numUnits
+
     def addInstruction(self, instruction):
         self.instructions.append( instruction )
+
+    def incInstruction(self):
+        if self.currInstruction < len(self.instructions) - 1:
+            self.currInstruction += 1
+        else:
+            self.finished = True
 
     def getID(self):
         return self.id
@@ -24,12 +48,6 @@ class Task:
             self.finished = True; return
 
         return self.instructions[self.currInstruction]
-
-    def incInstruction(self):
-        if self.currInstruction < len(self.instructions) - 1:
-            self.currInstruction += 1
-        else:
-            self.finished = True
 
     def __str__(self):
         return str(self.__dict__)
