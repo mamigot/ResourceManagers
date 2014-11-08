a865d6123be21a4bfc843ead9e36af70264c9820
miguel5 <miguel.amigot@gmail.com>
2014-11-07 20:45:56 -0500

    Methods to control instructions in Task objects

--- Task.py
+++ Task.py
@@ -2,7 +2,11 @@ class Task:
 
     def __init__(self, id):
         self.id = id
-        self.instructions = [ ]; # All but 'initiate'
+        self.instructions = [ ]
+
+        self.finished = False
+        self.currInstruction = 0 # Used to iterate through instructions
+
 
     def addInstruction(self, instruction):
         self.instructions.append( instruction )
@@ -10,8 +14,25 @@ class Task:
     def getID(self):
         return self.id
 
-    def getInstructions(self):
+    def getAllInstructions(self):
         return self.instructions
+
+    def getCurrentInstruction(self):
+        if self.isFinished() or self.currInstruction >= len(self.instructions):
+            return
+
+        return self.instructions[self.currInstruction]
+
+
+    def incInstruction(self):
+        if self.currInstruction < len(self.instructions) - 1:
+            self.currInstruction += 1
+        else:
+            self.finished = True
+
+
+    def isFinished(self):
+        return self.finished
 
     def __str__(self):
         return str(self.__dict__)
