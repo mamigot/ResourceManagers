5996bf4b0e75644b1b67eb6a407163459726a6a0
miguel5 <miguel.amigot@gmail.com>
2014-11-07 20:46:32 -0500

    Groundwork for the manager to execute instructions

--- Manager.py
+++ Manager.py
@@ -9,6 +9,7 @@ from Instruction import Instruction
 tasks = {} # Maps task IDs to Task objects
 resources = {} # Maps resource IDs to Resource objects
 
+sysClock = 0;
 
 
 def parseInputData(outline, instructions):
@@ -31,13 +32,51 @@ def parseInputData(outline, instructions):
         ins = Instruction(command, taskID, delay, resourceType, numUnits)
         tasks[taskID].addInstruction( ins )
 
+def isFinished():
+    for task in tasks.values():
+        if not task.isFinished():
+            return False
+    return True
+
+def execute(task, instruction):
+    if( instruction.delay ):
+        instruction.delay -= 1; return
+
+    if( instruction.command == "initiate" ):
+        print("initiate !!! yeh!")
+
+    if( instruction.command == "request" ):
+        print("requestttttt")
+
+    elif( instruction.command == "release" ):
+        print("release bruh")
+
+    task.incInstruction()
+
+
+
+def runManager():
+    global sysClock
+
+    while not isFinished():
+
+        for task in tasks.values():
+            ins = task.getCurrentInstruction()
+            execute(task, ins)
+
+        sysClock += 1
+
 
 
 if __name__ == "__main__":
-    filePath = "inputs/input-10.txt"
+    filePath = "inputs/input-01.txt"
     file = file(filePath, 'r')
 
     outline = [int(s) for s in file.readline().split()]
     instructions = re.findall(r'[a-z]+\s+[\d\s]+', file.read())
 
     parseInputData(outline, instructions)
+
+    runManager()
+
+    print(sysClock)
