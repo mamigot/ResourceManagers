630986bf8284292c42448570b4dd3a4694216fea
miguel5 <miguel.amigot@gmail.com>
2014-11-07 20:46:45 -0500

    Unnecessary parameters at the moment

--- Resource.py
+++ Resource.py
@@ -3,9 +3,5 @@ class Resource:
     def __init__(self, id, totUnits):
         self.id = id
 
-        self.totUnits = totUnits
-        self.available = totUnits
-        self.busy = 0
-
     def __repr__(self):
         return str(self.__dict__)
